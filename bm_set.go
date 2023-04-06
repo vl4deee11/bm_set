c2079adc764df37050645e5639d4d8657a121147
vl4deee11 <vl4deee11@gmail.com>
2023-04-06 14:36:34 +0400

    FEATURE: UNION & INTERSECT

--- bm_set.go
+++ bm_set.go
@@ -9,6 +9,8 @@ type SetI interface {
 	Set(i int)
 	Get(i int) bool
 	Delete(i int)
+	Intersect(oth *BMSet) SetI
+	Union(oth *BMSet) SetI
 }
 
 func New(size uint64) SetI {
@@ -57,4 +59,40 @@ func (s *BMSet) getSettings(i int) (int, uint64) {
 	}
 
 	return bn, uint64(1 << (i % 64))
+}
+
+func (s *BMSet) Intersect(oth *BMSet) SetI {
+	ll := len(oth.masks)
+	if len(s.masks) > ll {
+		ll = len(s.masks)
+	}
+	masks := make([]uint64, ll)
+	for i := 0; i < ll; i++ {
+		if i < len(s.masks) && i < len(oth.masks) {
+			masks[i] = s.masks[i] | oth.masks[i]
+		} else if i < len(s.masks) {
+			masks[i] = s.masks[i]
+		} else if i < len(oth.masks) {
+			masks[i] = oth.masks[i]
+		}
+	}
+	if s.zero || oth.zero {
+		return &BMSet{zero: true, masks: masks}
+	}
+	return &BMSet{zero: false, masks: masks}
+}
+
+func (s *BMSet) Union(oth *BMSet) SetI {
+	ll := len(oth.masks)
+	if len(s.masks) < ll {
+		ll = len(s.masks)
+	}
+	masks := make([]uint64, ll)
+	for i := 0; i < ll; i++ {
+		masks[i] = s.masks[i] & oth.masks[i]
+	}
+	if s.zero && oth.zero {
+		return &BMSet{zero: true, masks: masks}
+	}
+	return &BMSet{zero: false, masks: masks}
 }
--- bm_set_test.go
+++ bm_set_test.go
@@ -125,3 +125,63 @@ func TestBMSet2(t *testing.T) {
 		}
 	}
 }
+
+func TestIntersect(t *testing.T) {
+	bm1 := New(128)
+	bm1.Set(3)
+	bm1.Set(65)
+	bm1.Set(120)
+	bm1.Set(0)
+	bm1.Set(4)
+
+	bm2 := New(67)
+	bm2.Set(3)
+	bm2.Set(66)
+
+	bm3 := bm1.Intersect(bm2.(*BMSet))
+	vals := []int{3, 65, 0, 4, 66, 120}
+	for i := 0; i < len(vals); i++ {
+		if !bm3.Get(vals[i]) {
+			t.Errorf("test bms.Get(i) fail on = %d", vals[i])
+			return
+		}
+		bm3.Delete(vals[i])
+	}
+
+	for i := 0; i < 128; i++ {
+		if bm3.Get(i) {
+			t.Errorf("test bms.Get(i) fail on = %d", i)
+			return
+		}
+	}
+}
+
+func TestUnion(t *testing.T) {
+	bm1 := New(128)
+	bm1.Set(3)
+	bm1.Set(66)
+	bm1.Set(120)
+	bm1.Set(0)
+	bm1.Set(4)
+
+	bm2 := New(67)
+	bm2.Set(3)
+	bm2.Set(66)
+
+	bm3 := bm1.Union(bm2.(*BMSet))
+	vals := []int{3, 66}
+	for i := 0; i < len(vals); i++ {
+		if !bm3.Get(vals[i]) {
+			t.Errorf("test bms.Get(i) fail on = %d", vals[i])
+			return
+		}
+		bm3.Delete(vals[i])
+	}
+
+	for i := 0; i < 67; i++ {
+		if bm3.Get(i) {
+			t.Errorf("test bms.Get(i) fail on = %d", i)
+			return
+		}
+	}
+}
